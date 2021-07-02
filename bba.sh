e6d817516e9dd594b0dd62b07f6f04815345775f
ap <64496315+ap062@users.noreply.github.com>
2021-07-02 09:34:38 +0530

    Update bba.sh

--- bba.sh
+++ bba.sh
@@ -1,7 +1,7 @@
 domain=$1
-wordlist="/home/sc/wl.txt"
-reso="/home/sc/resolvers.txt"
-nuc="/home/sc/nuclei-templates"
+wordlist="/home/ap/need/wordlist.txt"
+reso="/home/ap/need/reso.txt"
+
 
 
 domain_enum(){
@@ -9,42 +9,41 @@ domain_enum(){
 mkdir -p $domain $domain/sources $domain/Recon  $domain/Recon/nuclei $domain/Recon/waybackurls  $domain/Recon/gf $domain/Recon/wordlist $domain/Recon/massscan
 
 subfinder -d $domain -o $domain/sources/subfinder.txt
-
+findomain -t  $domain  --quiet | tee $domain/sources/findodmain.txt
 assetfinder -subs-only $domain | tee $domain/sources/assetfinder.txt
 amass enum  -passive -d $domain -o $domain/sources/passive.txt
 shuffledns  -d $domain -w $wordlist -r $reso -o $domain/sources/shuffledns.txt
-
 cat $domain/sources/*.txt > $domain/sources/all.txt
 
 }
 domain_enum
 
 resolving_domains(){
-
+#resolve doamin using shuffle_dns
 shuffledns -d $domain -list $domain/sources/all.txt -o $domain/domains.txt  -r $reso
 }
 resolving_domains
 
 http_prob(){
-
+#checking http /https
 cat $domain/domains.txt | httpx -threads 200 -o $domain/Recon/httpx.txt
 }
 http_prob
 
 scanner(){
 
-cat $domain/Recon/httpx.txt |nuclei -t $nuc/cves/ -c 50 -o $domain/Recon/nuclei/cves.txt
+#cat $domain/Recon/httpx.txt |nuclei -t /home/ap/nuclei-templates/cves/ -c 50 -o $domain/Recon/nuclei/cves.txt
 cat $domain/Recon/httpx.txt |nuclei -t $nuc/files/ -c 50 -o $domain/Recon/nuclei/files.txt
-cat $domain/Recon/httpx.txt |nuclei -t $nuc/vulnerabilities/ -c 50 -o $domain/Recon/nuclei/vulnerabilites.txt
-cat $domain/Recon/httpx.txt |nuclei -t $nuc/technologies/ -c 50 -o $domain/Recon/nuclei/technologies.txt
+#cat $domain/Recon/httpx.txt |nuclei -t $nuc/vulnerabilities/ -c 50 -o $domain/Recon/nuclei/vulnerabilites.txt
+#cat $domain/Recon/httpx.txt |nuclei -t $nuc/technologies/ -c 50 -o $domain/Recon/nuclei/technologies.txt
 
 }
 scanner
 
 wbs(){
 
-waybackurls $domain | tee $domain/Recon/waybackurls/waybackurls.txt
-cat $domain/Recon/waybackurls/waybackurls.txt | egrep -v  "\.woff|\.ttf|\.eot|\.png|\.jpeg|\.jpg|\.svg|\.css|\.ico" |sed 's/:80//g;s/:443//g' | sort -u > $domain/Recon/waybackurls/wbvalid.txt
+cat $domain/Recon/httpx.txt | waybackurls | tee $domain/Recon/waybackurls/waybackurls.txt
+cat $domain/Recon/waybackurls/waybackurls.txt | egrep -v  "\.woff|\.ttf|\.eot|\.png|\.jpeg|\.jpg|\.svg|\.css|\.ico" |sed 's/:80//g;s/:443//g' | sort -u > $domain/Recon/waybackurls/wbresolve.txt
 
 
 }
@@ -52,8 +51,9 @@ wbs
 
 ff(){
 
-ffuf -c -u "FUZZ" -w  $domain/Recon/waybackurls/wbvalid.txt  -of csv -o $domain/Recon/waybackurls/vaild-temp.txt
-cat  $domain/Recon/waybackurls/vaild-temp.txt | grep http |awk -F "," '{print $1}' >> $domain/Recon/waybackurls/valid.txt
+ffuf -c -u "FUZZ" -w  $domain/Recon/waybackurls/wbresolve.txt -of csv -o $domain/Recon/waybackurls/temp.txt
+cat  $domain/Recon/waybackurls/temp.txt | grep http |awk -F '{print $3}' |tee $domain/Recon/waybackurls/valid.txt
+rm -rf $domain/Recon/waybackurls/temp.txt
 
 
 }
@@ -70,15 +70,17 @@ gfp
 
 
 cw(){
-cat $domain/Recon/waybackurls/wbvalid.txt | unfurl -unique paths > $domain/Recon/wordlist/path.txt
-cat $domain/Recon/waybackurls/wbvalid.txt | unfurl -unique keys > $domain/Recon/wordlist/params.txt
+cat $domain/Recon/waybackurls/valid.txt | unfurl -unique paths > $domain/Recon/wordlist/path.txt
+cat $domain/Recon/waybackurls/valid.txt | unfurl -unique keys > $domain/Recon/wordlist/params.txt
 }
 cw
 
 resolving(){
 
-massdns -r $reso  -t A -o S -w $domain/Recon/massscan/results.txt   $domain/domains.txt
-gf ip $domain/Recon/massscan/results.txt | sort -u > $domain/Recon/massscan/ip.txt
+massdns -r $reso  -t AAAA  -w $domain/Recon/massscan/results.txt   $domain/domains.txt
+cat $domain/Recon/massscan/results.txt |awk -F '{print $1}' |tee $domain/Recon/massscan/tmp.txt
+gf ip $domain/Recon/massscan/tmp.txt |tee $domain/Recon/massscan/ip.txt
+rm -rf $domain/Recon/massscan/tmp.txt
 
 }
 resolving
